843f10a80e33b791d35b0399d6adc318b52b40ba
Harim Luna <harimluna46@gmail.com>
2024-11-21 14:17:09 -0800

    interfaz update

--- Interfaz.py
+++ Interfaz.py
@@ -22,7 +22,7 @@ class CameraApp:
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(
             static_image_mode=False,
-            max_num_hands=1,
+            max_num_hands=2,
             min_detection_confidence=0.7,
             min_tracking_confidence=0.5
         )
@@ -137,21 +137,33 @@ class CameraApp:
             d = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
             return d
         if len(landmark_point) >= 21:
+
+            thumb_cmc = landmark_point[1]
             thumb_mcp = landmark_point[2]
             thumb_ip = landmark_point[3]
             thumb_tip = landmark_point[4]
-            index_finger_tip = landmark_point[8]
-            middle_finger_mcp = landmark_point[9]
-            index_finger_pip = landmark_point[6]
-            middle_finger_tip = landmark_point[12]
-            middle_finger_pip = landmark_point[10]
-            ring_finger_tip = landmark_point[16]
-            ring_finger_pip = landmark_point[14]
-            pinky_tip = landmark_point[20]
-            pinky_pip = landmark_point[18]
-            ring_finger_mcp = landmark_point[13]  # MCP del dedo anular
-            pinky_mcp = landmark_point[17]
 
+            index_finger_mcp = landmark_point[5]
+            index_finger_pip = landmark_point[6]
+            index_finger_dip = landmark_point[7]
+            index_finger_tip = landmark_point[8]
+
+            middle_finger_mcp = landmark_point[9]
+            middle_finger_pip = landmark_point[10]
+            middle_finger_dip = landmark_point[10]
+            middle_finger_tip = landmark_point[12]
+
+            ring_finger_mcp = landmark_point[13]
+            ring_finger_pip = landmark_point[14]
+            ring_finger_dip = landmark_point[15]
+            ring_finger_tip = landmark_point[16]
+
+            pinky_mcp = landmark_point[17]
+            pinky_pip = landmark_point[18]
+            pinky_dip = landmark_point[19]
+            pinky_tip = landmark_point[20]
+
+            distancia_referencia = distancia_euclidiana(index_finger_mcp, index_finger_tip)
             # Letra A (ya implementada)
             if abs(thumb_tip[1] - index_finger_pip[1]) < 45 \
                     and abs(thumb_tip[1] - middle_finger_pip[1]) < 30 \
@@ -159,10 +171,11 @@ class CameraApp:
                     and abs(thumb_tip[1] - pinky_pip[1]) < 30:
                 return "A"
 
-            # Letra B (implementada según los criterios que mencionaste)
+            # Letra B modificada
             elif index_finger_pip[1] - index_finger_tip[1] > 0 and pinky_pip[1] - pinky_tip[1] > 0 and \
                     middle_finger_pip[1] - middle_finger_tip[1] > 0 and ring_finger_pip[1] - ring_finger_tip[1] > 0 and \
-                    middle_finger_tip[1] - ring_finger_tip[1] < 0 and abs(thumb_tip[1] - ring_finger_mcp[1]) < 40:
+                    middle_finger_tip[1] - ring_finger_tip[1] < 0 and abs(
+                thumb_tip[1] - middle_finger_mcp[1]) < 10:  # Pulgar tocando el MCP del dedo medio
                 return "B"
             # Letra C (implementada según los criterios que mencionaste)
             elif abs(index_finger_tip[1] - thumb_tip[1]) < 360 and \
@@ -175,15 +188,16 @@ class CameraApp:
                  pinky_pip[1] - pinky_tip[1] < 0 < index_finger_pip[1] - index_finger_tip[1]:
                 return "D"
                 # Letra E (implementada según los criterios proporcionados)
-            elif index_finger_pip[1] - index_finger_tip[1] < 0 and \
-                 pinky_pip[1] - pinky_tip[1] < 0 and \
-                 middle_finger_pip[1] - middle_finger_tip[1] < 0 and \
-                 ring_finger_pip[1] - ring_finger_tip[1] < 0 and \
-                 abs(index_finger_tip[1] - thumb_tip[1]) < 100 and \
-                 thumb_tip[1] - index_finger_tip[1] > 0 and \
-                 thumb_tip[1] - middle_finger_tip[1] > 0 and \
-                 thumb_tip[1] - ring_finger_tip[1] > 0 and \
-                 thumb_tip[1] - pinky_tip[1] > 0:
+            elif abs(index_finger_tip[0] - index_finger_mcp[0]) < 20 and \
+                     abs(index_finger_tip[1] - index_finger_mcp[1]) < 20 and \
+                     abs(middle_finger_tip[0] - middle_finger_mcp[0]) < 20 and \
+                     abs(middle_finger_tip[1] - middle_finger_mcp[1]) < 20 and \
+                     abs(ring_finger_tip[0] - ring_finger_mcp[0]) < 20 and \
+                     abs(ring_finger_tip[1] - ring_finger_mcp[1]) < 20 and \
+                     abs(pinky_tip[0] - pinky_mcp[0]) < 20 and \
+                     abs(pinky_tip[1] - pinky_mcp[1]) < 20 and \
+                     abs(index_finger_tip[0] - thumb_tip[0]) < 20 and \
+                     abs(index_finger_tip[1] - thumb_tip[1]) < 20:
                 return "E"
             elif pinky_pip[1] - pinky_tip[1] > 0 and \
                     middle_finger_pip[1] - middle_finger_tip[1] > 0 and \
@@ -200,17 +214,172 @@ class CameraApp:
                     pinky_tip[1] > pinky_mcp[1]:
                 return "G"
 
+            elif thumb_tip[1] < thumb_mcp[1] and \
+                    abs(index_finger_tip[1] - index_finger_pip[1]) < 30 and \
+                    abs(middle_finger_tip[1] - middle_finger_pip[1]) < 30 and \
+                    ring_finger_tip[1] > ring_finger_pip[1] and \
+                    pinky_tip[1] > pinky_pip[1] and \
+                    thumb_tip[0] < index_finger_pip[0] and \
+                    abs(index_finger_tip[0] - middle_finger_tip[0]) < 40:  # Índice y medio horizontales y juntos
+                return "H"
+
+            elif abs(pinky_tip[1] - pinky_mcp[1]) > 30 and \
+                    abs(index_finger_mcp[1] - middle_finger_mcp[1]) < 20 and \
+                    abs(middle_finger_mcp[1] - ring_finger_mcp[1]) < 20 and \
+                    index_finger_tip[1] > index_finger_mcp[1] and \
+                    thumb_tip[1] < ring_finger_tip[1] and \
+                    thumb_tip[1] > ring_finger_pip[1] and \
+                    thumb_tip[0] < index_finger_mcp[0] + 15:  # Condiciones específicas para la letra I
+                return "I"
+            elif abs(pinky_tip[1] - pinky_mcp[1]) > 30 and \
+                    abs(index_finger_mcp[1] - middle_finger_mcp[1]) < 20 and \
+                    abs(middle_finger_mcp[1] - ring_finger_mcp[1]) < 20 and \
+                    index_finger_tip[1] > index_finger_mcp[1] and \
+                    thumb_tip[1] < ring_finger_tip[1] and \
+                    thumb_tip[1] > ring_finger_pip[1] and \
+                    abs(thumb_tip[0] - index_finger_mcp[0]) > 20:  # Condiciones específicas para la letra J
+                return "J"
+            elif abs(index_finger_mcp[1] - index_finger_pip[1]) < distancia_referencia * 0.2 and \
+                    abs(index_finger_pip[1] - index_finger_dip[1]) < distancia_referencia * 0.2 and \
+                    abs(index_finger_dip[1] - index_finger_tip[1]) < distancia_referencia * 0.2 and \
+                    middle_finger_tip[1] > middle_finger_pip[1] and \
+                    middle_finger_pip[1] > middle_finger_mcp[1] and \
+                    abs(middle_finger_mcp[0] - middle_finger_tip[
+                        0]) < distancia_referencia * 0.4:  # Curvatura del dedo medio
+                return "K"
+
+            # Letra L
+            elif abs(index_finger_mcp[1] - index_finger_pip[1]) > 40 and \
+                    abs(index_finger_pip[1] - index_finger_dip[1]) > 40 and \
+                    abs(index_finger_dip[1] - index_finger_tip[1]) > 40 and \
+                    abs(thumb_cmc[0] - thumb_tip[0]) > 40 and \
+                    abs(thumb_mcp[0] - thumb_tip[0]) > 40 and \
+                    abs(thumb_tip[1] - index_finger_mcp[1]) < 50 and \
+                    middle_finger_tip[1] > middle_finger_mcp[1] + 20 and \
+                    ring_finger_tip[1] > ring_finger_mcp[1] + 20 and \
+                    pinky_tip[1] > pinky_mcp[1] + 20:  # Solo índice y pulgar extendidos, el resto de los dedos doblados
+                return "L"
+            # Letra M
+            elif index_finger_tip[1] < index_finger_mcp[1] and \
+                    middle_finger_tip[1] < middle_finger_mcp[1] and \
+                    ring_finger_tip[1] < ring_finger_mcp[1] and \
+                    abs(index_finger_tip[0] - index_finger_mcp[0]) < 30 and \
+                    abs(middle_finger_tip[0] - middle_finger_mcp[0]) < 30 and \
+                    abs(ring_finger_tip[0] - ring_finger_mcp[0]) < 30 and \
+                    abs(thumb_tip[0] - pinky_tip[0]) < 20 and \
+                    thumb_tip[1] < pinky_tip[1] and \
+                    thumb_tip[0] < pinky_tip[0]:  # El pulgar debe estar tocando el meñique
+                return "M"
+
+                # Letra N
+            elif ring_finger_tip[1] > ring_finger_mcp[1] + 30 and \
+                 middle_finger_tip[1] > middle_finger_mcp[1] + 30 and \
+                 index_finger_tip[1] > index_finger_mcp[1] + 30 and \
+                 thumb_tip[1] < index_finger_pip[1] - 30 and \
+                 thumb_tip[1] < middle_finger_pip[1] - 30 and \
+                 thumb_tip[1] < ring_finger_pip[1] - 30:  # El pulgar debe cubrir las puntas de los dedos índice, medio y anular con mayor flexibilidad
+
+                 return "N"
+
+
+            elif abs(index_finger_tip[0] - thumb_tip[0]) < 50 and \
+                 abs(index_finger_tip[1] - thumb_tip[1]) < 50 and \
+                 abs(middle_finger_tip[0] - thumb_tip[0]) < 50 and \
+                 abs(middle_finger_tip[1] - thumb_tip[1]) < 50 and \
+                 abs(ring_finger_tip[0] - thumb_tip[0]) < 50 and \
+                 abs(ring_finger_tip[1] - thumb_tip[1]) < 50 and \
+                 abs(pinky_tip[0] - thumb_tip[0]) < 50 and \
+                abs(pinky_tip[1] - thumb_tip[1]) < 50:  # Los dedos deben estar curvados y tocando la punta del pulgar
+
+                return "O"
+            # Letra P Modificar
+            elif abs(index_finger_tip[1] - index_finger_mcp[1]) > 40 and \
+                    abs(middle_finger_tip[1] - middle_finger_mcp[1]) > 40 and \
+                    ring_finger_tip[1] < ring_finger_mcp[1] and \
+                    pinky_tip[1] < pinky_mcp[1] and \
+                    thumb_tip[1] < index_finger_mcp[1] and \
+                    thumb_tip[0] < index_finger_mcp[0] - 15:  # Pulgar doblado, índice y medio extendidos hacia arriba
+                return "P"
+            # Letra R
+            elif abs(index_finger_tip[1] - index_finger_mcp[1]) > 40 and \
+                    abs(middle_finger_tip[1] - middle_finger_mcp[1]) > 40 and \
+                    abs(index_finger_tip[0] - middle_finger_tip[0]) < 10 and \
+                    abs(index_finger_tip[1] - middle_finger_tip[1]) < 10 and \
+                    ring_finger_tip[1] > ring_finger_mcp[1] + 20 and \
+                    pinky_tip[1] > pinky_mcp[1] + 20 and \
+                    thumb_tip[1] > index_finger_mcp[
+                1]:  # Índice y medio tocándose, otros dedos doblados, pulgar alejado
+                return "R"
+            # Letra S
+            elif index_finger_tip[1] > index_finger_mcp[1] + 20 and \
+                    middle_finger_tip[1] > middle_finger_mcp[1] + 20 and \
+                    ring_finger_tip[1] > ring_finger_mcp[1] + 20 and \
+                    pinky_tip[1] > pinky_mcp[1] + 20 and \
+                    thumb_tip[1] > index_finger_pip[1] + 10 and \
+                    thumb_tip[0] > index_finger_mcp[
+                0] - 10:  # El pulgar está sobre los dedos y alineado ligeramente hacia la derecha
+                return "S"
+            # Letra U
+            elif index_finger_tip[1] < index_finger_pip[1] and \
+                    middle_finger_tip[1] < middle_finger_pip[1] and \
+                    ring_finger_tip[1] > ring_finger_pip[1] and \
+                    pinky_tip[1] > pinky_pip[1] and \
+                    abs(index_finger_tip[0] - middle_finger_tip[0]) < 40 and \
+                    thumb_tip[1] > index_finger_mcp[1]:  # El pulgar permanece abajo y lejos de los dedos levantados
+                return "U"
+            # Letra V
+            elif index_finger_tip[1] < index_finger_pip[1] and \
+                    middle_finger_tip[1] < middle_finger_pip[1] and \
+                    ring_finger_tip[1] > ring_finger_pip[1] and \
+                    pinky_tip[1] > pinky_pip[1] and \
+                    abs(index_finger_tip[0] - middle_finger_tip[0]) > 40 and \
+                    thumb_tip[1] > index_finger_mcp[1]:  # El pulgar permanece abajo y separado de los dedos levantados
+                return "V"
+            # Letra W
+            elif index_finger_tip[1] < index_finger_pip[1] and \
+                    middle_finger_tip[1] < middle_finger_pip[1] and \
+                    ring_finger_tip[1] < ring_finger_pip[1] and \
+                    pinky_tip[1] > pinky_pip[1] and \
+                    abs(index_finger_tip[0] - middle_finger_tip[0]) > 40 and \
+                    abs(middle_finger_tip[0] - ring_finger_tip[0]) > 40 and \
+                    thumb_tip[1] > index_finger_mcp[1]:  # Pulgar abajo y no interferente
+                return "W"
+            # Letra X
+            elif index_finger_tip[1] > index_finger_pip[1] and \
+                    middle_finger_tip[1] > middle_finger_pip[1] and \
+                    ring_finger_tip[1] > ring_finger_pip[1] and \
+                    pinky_tip[1] > pinky_pip[1] and \
+                    thumb_tip[0] < index_finger_mcp[0] and \
+                    abs(index_finger_tip[0] - thumb_tip[0]) < 30:  # Pulgar cerca del índice formando un cruce
+                return "X"
+            # Letra Y
+            elif index_finger_tip[1] > index_finger_pip[1] and \
+                    middle_finger_tip[1] > middle_finger_pip[1] and \
+                    ring_finger_tip[1] > ring_finger_pip[1] and \
+                    pinky_tip[1] < pinky_pip[1] and \
+                    thumb_tip[1] < thumb_ip[1]:  # El pulgar y el meñique están hacia arriba
+                return "Y"
+            # Letra Z
+            elif index_finger_tip[1] > index_finger_pip[1] and \
+                    middle_finger_tip[1] > middle_finger_pip[1] and \
+                    ring_finger_tip[1] > ring_finger_pip[1] and \
+                    pinky_tip[1] < pinky_pip[1] and \
+                    thumb_tip[1] > thumb_ip[1]:  # El meñique está hacia abajo y el pulgar hacia arriba
+                return "Z"
         return None
 
     def draw_landmarks(self, image, landmark_point):
         if len(landmark_point) > 0:
             # Dibujar líneas del esqueleto
             for i, j in [
-                (0, 1), (1, 2), (2, 3), (3, 4),  # Pulgar
-                (0, 5), (5, 6), (6, 7), (7, 8),  # Índice
-                (0, 9), (9, 10), (10, 11), (11, 12),  # Medio
-                (0, 13), (13, 14), (14, 15), (15, 16),  # Anular
-                (0, 17), (17, 18), (18, 19), (19, 20)  # Meñique
+                (0, 1), (1, 2), (2, 3), (3, 4), (2, 5), # Pulgar
+                (0, 17), (17, 18), (18, 19), (19, 20),  # Meñique
+                (5, 9), (9, 13),(13, 17),# palma
+                (0, 5), (5, 6),(6, 7),(7, 8),#indice
+                (9, 10), (10, 11), (11, 12), #medio
+                (13, 14), (14, 15), (15, 16),  #anular
+
+
             ]:
                 cv2.line(image, tuple(landmark_point[i]), tuple(landmark_point[j]), (0, 0, 255), 2)
 
@@ -221,9 +390,10 @@ class CameraApp:
                 cv2.circle(image, tuple(landmark), radius, (0, 0, 0), 1)
         return image
 
+
     def update_chat_with_delay(self, message):
         current_time = time.time()
-        if current_time - self.last_added_time >= 1:  # 1 segundos de intervalo
+        if current_time - self.last_added_time >= 2:  # 1 segundos de intervalo
             self.add_to_chat(message)
             self.last_added_time = current_time
 
